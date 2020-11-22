14ffc93f664f63a85c7cd470593048b551a0efb9
Joel Okoro <jukachi95@gmail.com>
2020-11-22 21:15:38 +0000

    Created generateHTML helper function

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-let url = 'https://randomuser.me/api/?results=12'
+let url = 'https://randomuser.me/api/?results=12';
+let card = document.querySelector('.card')
 let gallery = document.getElementById('gallery');
 let modalContainer = document.querySelector('modal-container')
 
-// Fetch the initial information
+// Use the Fetch API to get the initial employee information
+// Access the json data
+// Map the data to access the different key/value pairs
 
 fetch(url)
 .then(res =>  res.json())
 .then(data => data.results.map(info => {
-    console.log(info.picture, info.name.first, info.name.second, info.location, info.email)
+    generateHTML(info)
 }))
 
 
 // Helper functions
+
+// Generate HTML
+function generateHTML(item){
+
+    // Create a loop for each item in the array
+    // Append to the HTML card item
+
+
+}
