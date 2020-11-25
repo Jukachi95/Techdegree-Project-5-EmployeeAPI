5c3b0635a13d4ab79da6c11d4d163b554771cbbe
Joel Okoro <jukachi95@gmail.com>
2020-11-25 17:44:31 +0000

    Added function for creation of card items and addition to fetch api

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let url = 'https://randomuser.me/api/?results=12';
-let card = document.querySelector('.card')
+// let card = document.querySelectorAll('.card')
 let gallery = document.getElementById('gallery');
 let modalContainer = document.querySelector('modal-container')
+
+
 
 // Use the Fetch API to get the initial employee information
 // Access the json data
@@ -9,9 +11,8 @@ let modalContainer = document.querySelector('modal-container')
 
 fetch(url)
 .then(res =>  res.json())
-.then(data => data.results.map(info => {
-    generateHTML(info)
-}))
+.then(data => generateHTML(data.results))
+.then(()=> createCard())
 
 
 // Helper functions
@@ -21,6 +22,71 @@ function generateHTML(item){
 
     // Create a loop for each item in the array
     // Append to the HTML card item
+    
+
+    let html = ''
+
+    for(let i = 0; i < item.length; i++){
+        console.log(item[i])
+        if(item[i]){
+            html += `
+            
+            <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${item[i].picture.large}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">${item[i].name.first} ${item[i].name.last}</h3>
+                <p class="card-text">${item[i].email}</p>
+                <p class="card-text cap">${item[i].location.city}</p>
+            </div>
+        </div>
+            
+            `
+        }
+    }
+
+    gallery.innerHTML = html
 
 
 }
+
+// When button is clicked, modal appears
+// for loop and iteration for card[i]??
+
+function createCard(){
+    let card = document.querySelectorAll('.card')
+
+    for(let i = 0; i < card.length; i++){
+        card[i].addEventListener('click', ()=>{
+            console.log("Hello")
+        })
+    }
+    
+}
+
+
+function createModal(info){
+    
+let modal = `
+
+ <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">name</h3>
+                        <p class="modal-text">email</p>
+                        <p class="modal-text cap">city</p>
+                        <hr>
+                        <p class="modal-text">(555) 555-5555</p>
+                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                        <p class="modal-text">Birthday: 10/21/2015</p>
+                    </div>
+                </div>
+`
+body += modal
+
+}
+
+
