199be26cc7f9589cfc483abe3198a6b099962d9b
Joel Okoro <jukachi95@gmail.com>
2020-11-25 22:21:59 +0000

    Added code for e.composedPath

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
+// Declare variables
+
 let url = 'https://randomuser.me/api/?results=12';
-// let card = document.querySelectorAll('.card')
 let gallery = document.getElementById('gallery');
 let modalContainer = document.querySelector('modal-container')
-
-
+let body = document.querySelector('body')
 
 // Use the Fetch API to get the initial employee information
 // Access the json data
-// Map the data to access the different key/value pairs
+// Use a helper function to generate the HTML for gallery and cards
 
 fetch(url)
 .then(res =>  res.json())
 .then(data => generateHTML(data.results))
-.then(()=> createCard())
+.then(() => generateModal())
 
 
-// Helper functions
+//  ** Helper functions ** //
 
 // Generate HTML
 function generateHTML(item){
 
     // Create a loop for each item in the array
     // Append to the HTML card item
-    
 
+    
+    
+    // Declare html variable
     let html = ''
 
+    // Loop through the items passed in to function, in this case data.results
     for(let i = 0; i < item.length; i++){
-        console.log(item[i])
+        
+        // If there is an item in the array...
         if(item[i]){
+
+            // Append the 'card' html markup to the html variable
+            // And interpolate the information that was passed in as an argument using template strings
             html += `
-            
             <div class="card">
             <div class="card-img-container">
                 <img class="card-img" src="${item[i].picture.large}" alt="profile picture">
@@ -46,47 +52,30 @@ function generateHTML(item){
         }
     }
 
+    // Append the html variable to the gallery
     gallery.innerHTML = html
 
 
 }
 
-// When button is clicked, modal appears
-// for loop and iteration for card[i]??
+// When a card or any item in the card is clicked, modal appears
 
-function createCard(){
+// Generate the modal
+function generateModal(info){
     let card = document.querySelectorAll('.card')
 
     for(let i = 0; i < card.length; i++){
-        card[i].addEventListener('click', ()=>{
-            console.log("Hello")
+        card[i].addEventListener('click', (e)=>{
+            
+            
+            console.log(e.composedPath())
+
+        
+
+        
         })
     }
     
-}
-
-
-function createModal(info){
-    
-let modal = `
-
- <div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">name</h3>
-                        <p class="modal-text">email</p>
-                        <p class="modal-text cap">city</p>
-                        <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
-                    </div>
-                </div>
-`
-body += modal
-
 }
 
 
